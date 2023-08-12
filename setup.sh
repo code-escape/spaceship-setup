e98d3dfc5bcf94f617e1c1ffc191c4817f7cf588
Adrien Saunier <adrien.saunier>
2023-08-12 13:12:40 +0200

    tech(): Clean the machine after de setup run

--- setup.sh
+++ setup.sh
@@ -20,7 +20,7 @@ curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo bash -
 sudo apt-get install -y nodejs
 
 echo -e "\n\n⚙️ - Install PM2"
-npm install -g pm2
+sudo npm install -g pm2
 npm install
 npm run build
 pm2 start ecosystem.config.js
@@ -28,33 +28,3 @@ pm2 stop all
 
 sudo rm -rf ./setup
 sudo rm setup.sh
-
-#KEYRING=/usr/share/keyrings/nodesource.gpg
-#wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | tee "$KEYRING" >/dev/null
-#gpg --no-default-keyring --keyring "$KEYRING" apt update && apt upgrade
-
-#echo "⚙️ - Update the image"
-#apt update
-#apt install git ufw sudo wget gpg -y
-#apt install -y --no-install-recommends apt-utils
-#
-#echo "⚙️ - Install nginx"
-#{
-#  apt install nginx -y
-#  ufw allow 'Nginx HTTP'
-#  service nginx start
-#} 1>/dev/null
-#
-## Git
-#echo "⚙️ - Install Git"
-#{
-#  git clone https://github.com/code-escape/spaceship-setup.git
-#} 1>/dev/null
-#
-#echo "⚙️ - Install NodeJs"
-#{
-#  KEYRING=/usr/share/keyrings/nodesource.gpg
-#  wget --quiet -O - https://deb.nodesource.com/gpgkey/nodesource.gpg.key | gpg --dearmor | tee "$KEYRING" >/dev/null
-#  gpg --no-default-keyring --keyring "$KEYRING" --list-keyssudo apt update && apt upgrade
-#} 1>/dev/null
-#
