a9fd65668a64b8dccd2849c7dfc1c38d15c56d88
Adrien Saunier <adrien.saunier>
2023-09-09 10:25:32 +0200

    tech/Use the new nodejs installation procedure

--- setup/run.sh
+++ setup/run.sh
@@ -17,8 +17,13 @@ sudo cp ./setup/nginx.conf ../nginx/nginx.conf
 sudo service nginx restart
 
 echo -e "\n\n⚙️ - Install NodeJs"
-curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+NODE_MAJOR=18
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+sudo apt-get update
+sudo apt-get install nodejs -y
 
 echo -e "\n\n⚙️ - Install PM2"
 sudo npm install -g pm2
